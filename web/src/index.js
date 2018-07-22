9a0ee51bfdc11732adaf401773a5a83614ad371c
Sean Kennedy <sean@kennedy.software>
2018-07-21 22:33:50 -0400

    Updated react version

--- web/src/index.js
+++ web/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'typeface-roboto'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
