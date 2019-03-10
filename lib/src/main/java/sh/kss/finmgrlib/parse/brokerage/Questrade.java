16d87718615b1fe2494e88aeee376e330353d68f
Sean Kennedy <sean@kennedy.software>
2019-03-10 03:19:14 -0400

    Ignore pdf files for now, simple implementation of Questrade detection and parsing

--- lib/src/main/java/sh/kss/finmgrlib/parse/brokerage/Questrade.java
+++ lib/src/main/java/sh/kss/finmgrlib/parse/brokerage/Questrade.java
@@ -20,21 +20,59 @@ package sh.kss.finmgrlib.parse.brokerage;
 import sh.kss.finmgrlib.entity.transaction.InvestmentTransaction;
 import sh.kss.finmgrlib.parse.Parser;
 
+import javax.money.CurrencyUnit;
+import javax.money.Monetary;
+import java.util.Collections;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Questrade extends Parser {
 
     @Override
     public boolean isMatch(List<String> lines) {
-        return false;
+
+        return lines.get(37).trim().endsWith("Questrade");
     }
 
     @Override
     public List<InvestmentTransaction> parse(List<String> lines) {
-        return null;
-    }
 
-    public boolean isTransaction(String line) {
-        return false;
+        try {
+            CurrencyUnit currency = Monetary.getCurrency("CAD");
+
+            for (String line : lines) {
+
+                Pattern transactionPattern = Pattern.compile("(?<transaction>\\d{1,2}/\\d{1,2}/\\d{4}) "
+                + "(?<settlement>\\d{1,2}/\\d{1,2}/\\d{4}) "
+                + "(?<type>\\w+) "
+                + "(?<description>.+) "
+                + "(?<quantity>[\\d|,]+) "
+                + "(?<price>\\$[\\d|,]+\\.\\d{3}) "
+                + "(?<gross>\\(?\\$[\\d|,]+\\.\\d{2}\\)?) "
+                + "(?<commission>\\(?\\$[\\d|,]+\\.\\d{2}\\)?) "
+                + "(?<net>\\(?\\$[\\d|,]+\\.\\d{2}\\)?)");
+
+                Matcher transaction = transactionPattern.matcher(line.trim());
+
+                if (transaction.find()) {
+
+                    System.out.println("transaction: " + transaction.group("transaction"));
+                    System.out.println("settlement: " + transaction.group("settlement"));
+                    System.out.println("type: " + transaction.group("type"));
+                    System.out.println("description: " + transaction.group("description"));
+                    System.out.println("quantity: " + transaction.group("quantity"));
+                    System.out.println("price: " + transaction.group("price"));
+                    System.out.println("gross: " + transaction.group("gross"));
+                    System.out.println("commission: " + transaction.group("commission"));
+                    System.out.println("net: " + transaction.group("net"));
+                }
+            }
+        } catch (NullPointerException npe) {
+
+            System.out.println(npe.getLocalizedMessage());
+        }
+
+        return Collections.emptyList();
     }
 }
