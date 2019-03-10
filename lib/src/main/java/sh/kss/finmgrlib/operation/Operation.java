8ba05af8cbfbfe019194dccf476f83f3e42eb744
Sean Kennedy <sean@kennedy.software>
2019-03-09 23:10:38 -0500

    Removed old operation execution architecture

--- lib/src/main/java/sh/kss/finmgrlib/operation/Operation.java
+++ lib/src/main/java/sh/kss/finmgrlib/operation/Operation.java
@@ -17,74 +17,10 @@
  */
 package sh.kss.finmgrlib.operation;
 
-import org.javamoney.moneta.Money;
-import org.springframework.stereotype.Service;
 import sh.kss.finmgrlib.entity.Portfolio;
 import sh.kss.finmgrlib.entity.transaction.InvestmentTransaction;
 
-import javax.money.CurrencyUnit;
-import javax.money.Monetary;
-import javax.money.MonetaryAmount;
-import java.math.BigDecimal;
-import java.util.List;
-
-@Service
 public abstract class Operation {
 
     abstract public Portfolio process(Portfolio portfolio, InvestmentTransaction investmentTransaction);
-
-    public static MonetaryAmount currentACB(List<InvestmentTransaction> transactions) {
-
-        // TODO: Implement base currency + auto conversion
-        CurrencyUnit currency = Monetary.getCurrency(transactions.get(0).getCurrency().getValue());
-
-        MonetaryAmount acb = Money.of(0, currency);
-        BigDecimal quantity = new BigDecimal("0");
-
-        for (InvestmentTransaction transaction : transactions) {
-
-            switch (transaction.getAction()) {
-
-                // ACB is summed with net amount of purchases
-                case Reinvest:
-                case Buy:
-                    acb = acb.add(transaction.getNetAmount());
-                    quantity = transaction.getQuantity().getValue().add(quantity);
-
-                    break;
-
-                // ACB per share remains constant during sales.
-                case Sell:
-                    MonetaryAmount acbPerShare = acb.divide(quantity);
-                    quantity = transaction.getQuantity().getValue().add(quantity);
-
-                    acb = acbPerShare.multiply(quantity);
-
-                    break;
-
-                // Return of Capital reduces ACB
-                case Distribution:
-                    MonetaryAmount subtrahend = transaction.getReturnOfCapital().multiply(quantity).negate();
-
-                    acb = acb.subtract(subtrahend);
-
-                    break;
-
-                default:
-                    break;
-            }
-
-            if (quantity.equals(BigDecimal.ZERO)) {
-
-                acb = Money.of(0, currency);
-            }
-        }
-
-        if (quantity.equals(BigDecimal.ZERO)) {
-
-            return acb;
-        }
-
-        return acb.divide(quantity).negate();
-    }
 }
